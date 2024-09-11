1d3724bdb7760eea6cd783636463baf656245f78
Lucian Green <luciangreen@lucianacademy.com>
2024-09-11 22:09:50 +1000

    Update grammar_logic_to_alg1.pl

--- grammar_logic_to_alg1.pl
+++ grammar_logic_to_alg1.pl
@@ -44,6 +44,7 @@ later: can randomly generate details like given sentence
 %:- include('../Philosophy/14 10 23.pl').
 
 :- dynamic brdict/1.
+:-dynamic words_for_sent/1.
 
 
 
@@ -110,16 +111,18 @@ grammar_logic_to_alg1(String1,N,Result) :-
 	grammar_logic_to_alg114(String,Result).
 	
 grammar_logic_to_alg1 :-
-	grammar_logic_to_alg11([Sentence1,List_a,List_a1,List_b1,List_bb,List_bb1,Cs1,Cs2]),
-	writeln1([*,Sentence1,a_alg(List_a)]),
-	writeln(List_a1),
-	writeln1([*,Sentence1,b_alg(List_a)]),
-	writeln(List_b1),
-	writeln1([*,Sentence1,bb_alg(List_bb)]),
-	writeln(List_bb1),
+	grammar_logic_to_alg11(S%[Sentence1,List_a,List_a1,List_b1,List_bb,List_bb1,Cs1,Cs2]
+	),
+	%writeln1([*,Sentence1,a_alg(List_a)]),
+	%writeln(List_a1),
+	%writeln1([*,Sentence1,b_alg(List_a)]),
+	%writeln(List_b1),
+	%writeln1([*,Sentence1,bb_alg(List_bb)]),
+	%writeln(List_bb1),
 
-	writeln1(Cs1),
-	writeln1(Cs2).
+	%writeln1(Cs1),
+	%writeln1(Cs2)
+	writeln(S).
 
 grammar_logic_to_alg112(Result) :-
 	grammar_logic_to_alg11(Result1),term_to_atom(Result1,Result).
@@ -128,13 +131,16 @@ grammar_logic_to_alg114(Text,Result) :-
 	grammar_logic_to_alg113(Text,Result1),term_to_atom(Result1,Result).
 
 
-grammar_logic_to_alg11([Sentence1,List_a,List_a1,List_b1,List_bb,List_bb1,Cs1,Cs2]) :-
+grammar_logic_to_alg11(S%[Sentence1,List_a,List_a1,List_b1,List_bb,List_bb1,Cs1,Cs2]
+) :-
 	phrase_from_file_s(string(Text1), "../Text-to-Breasonings/file.txt"),
 
-grammar_logic_to_alg113(Text1,[Sentence1,List_a,List_a1,List_b1,List_bb,List_bb1,Cs1,Cs2]).
+grammar_logic_to_alg113(Text1,S%[Sentence1,List_a,List_a1,List_b1,List_bb,List_bb1,Cs1,Cs2]
+).
 
 
-grammar_logic_to_alg113(Text1,[Sentence1,List_a,List_a1,List_b1,List_bb,List_bb1,Cs1,Cs2]) :-
+grammar_logic_to_alg113(Text1,S%[Sentence1,List_a,List_a1,List_b1,List_bb,List_bb1,Cs1,Cs2]
+) :-
 
 	phrase_from_file_s(string(BrDict0), "../Text-to-Breasonings/brdict1.txt"),
 	splitfurther(BrDict0,BrDict01),
@@ -147,36 +153,22 @@ grammar_logic_to_alg113(Text1,[Sentence1,List_a,List_a1,List_b1,List_bb,List_bb1
 	delete(Text2a,"",Text2),
 	
 	findall(B2,(member(B1,Text2),grammar_logic_to_alg(B1,B2)),C),
-	length(C,CLength),
-	writeln([CLength,sentences]),
-	
-	C=[[[_,Sentence1,a_alg(List_a),_,bb_alg(List_bb)]|Cs1]|Cs2],
-		%%get_time(TS),stamp_date_time(TS,date(Year,Month,Day,Hour1,Minute1,Seconda,_A,_TZ,_False),local),
-	concat_list(["gla_del"],%%[Year,Month,Day,Hour1,Minute1,Seconda],
-	File1),
-	concat_list(["\"",File1,".txt\""],File2),
 
-	term_to_atom(List_a,List_a2),
-	string_atom(List_a3,List_a2),
-	
-	concat_list(["swipl -G100g -T20g -L2g\n['../listprolog'].\nleash(-all),visible(+all),protocol(",File2,"),trace,interpret(off,[[n,function],[",List_a3,"]],[[[n,function],[[v,a]],\":-\",[[[n,length],[[v,a],0,1]]]],[[n,function],[[v,a]],\":-\",[[[n,head],[[v,a],[v,d]]],[[n,equals1],[[v,d],[[v,e],[v,f]]]],[[n,reverse],[[v,a],[],[v,a1]]],[[n,head],[[v,a1],[v,d1]]],[[n,equals1],[[v,d1],[[v,e1],[v,f1]]]],[[n,function2],[[v,a],[v,f],[v,f1]]]]],[[n,reverse],[[],[v,l],[v,l]]],[[n,reverse],[[v,l],[v,m],[v,n]],\":-\",[[[n,head],[[v,l],[v,h]]],[[n,tail],[[v,l],[v,t]]],[[n,wrap],[[v,h],[v,h1]]],[[n,append],[[v,h1],[v,m],[v,o]]],[[n,reverse],[[v,t],[v,o],[v,n]]]]],[[n,function2],[[v,a],[v,b],[v,f]],\":-\",[[[n,member2],[[v,a],[v,d]]],[[n,equals1],[[v,d],[[v,b],[v,f]]]]]],[[n,function2],[[v,a],[v,b],[v,c]],\":-\",[[[n,member2],[[v,a],[v,d]]],[[n,equals1],[[v,d],[[v,b],[v,f]]]],[[n,function2],[[v,d],[v,f],[v,c]]]]],[[n,length],[[],[v,l],[v,l]]],[[n,length],[[v,l],[v,m1],[v,n]],\":-\",[[[n,not],[[[n,=],[[v,l],[]]]]],[[n,tail],[[v,l],[v,t]]],[[n,+],[[v,m1],1,[v,m2]]],[[n,length],[[v,t],[v,m2],[v,n]]]]]],[[]]),notrace,noprotocol.\nhalt.\nswipl -G100g -T20g -L2g\n['../Text-to-Breasonings/text_to_breasonings.pl'].\ntime((N is 3,\nM is 16000,\ntexttobr2(N,",File2,",u,M),texttobr(N,",File2,",u,M))).\n['../texttobr2qb'].\ntexttobr2(3).\nhalt."],List_a1),
-
-	concat_list(["\n\nswipl -G100g -T20g -L2g\n['../listprolog'].\nleash(-all),visible(+all),protocol(",File2,"),trace,interpret(off,[[n,function],[",List_a3,",[v,b]]],[[[n,function],[[v,a],[v,b]],\":-\",[[[n,tail],[[v,a],[v,b]]]]]],_),notrace,noprotocol.\nhalt.\nswipl -G100g -T20g -L2g\n['../Text-to-Breasonings/text_to_breasonings.pl'].\ntime((N is 3,\nM is 16000,\ntexttobr2(N,",File2,",u,M),texttobr(N,",File2,",u,M))).\n['../texttobr2qb'].\ntexttobr2(3).\nhalt."],List_b1),
-
-	term_to_atom(List_bb,List_bb2),
-	string_atom(List_bb3,List_bb2),
-
-	concat_list(["swipl -G100g -T20g -L2g\n['../listprolog'].\nleash(-all),visible(+all),protocol(",File2,"),trace,interpret(off,[[n,function],[",List_bb3,"]],[[[n,function],[[v,a]],\":-\",[[[n,length],[[v,a],0,1]]]],[[n,function],[[v,a]],\":-\",[[[n,head],[[v,a],[v,d]]],[[n,equals1],[[v,d],[[v,e],[v,f]]]],[[n,reverse],[[v,a],[],[v,a1]]],[[n,head],[[v,a1],[v,d1]]],[[n,equals1],[[v,d1],[[v,e1],[v,f1]]]],[[n,function2],[[v,a],[v,f],[v,f1]]]]],[[n,reverse],[[],[v,l],[v,l]]],[[n,reverse],[[v,l],[v,m],[v,n]],\":-\",[[[n,head],[[v,l],[v,h]]],[[n,tail],[[v,l],[v,t]]],[[n,wrap],[[v,h],[v,h1]]],[[n,append],[[v,h1],[v,m],[v,o]]],[[n,reverse],[[v,t],[v,o],[v,n]]]]],[[n,function2],[[v,a],[v,b],[v,f]],\":-\",[[[n,member2],[[v,a],[v,d]]],[[n,equals1],[[v,d],[[v,b],[v,f]]]]]],[[n,function2],[[v,a],[v,b],[v,c]],\":-\",[[[n,member2],[[v,a],[v,d]]],[[n,equals1],[[v,d],[[v,b],[v,f]]]],[[n,function2],[[v,d],[v,f],[v,c]]]]],[[n,length],[[],[v,l],[v,l]]],[[n,length],[[v,l],[v,m1],[v,n]],\":-\",[[[n,not],[[[n,=],[[v,l],[]]]]],[[n,tail],[[v,l],[v,t]]],[[n,+],[[v,m1],1,[v,m2]]],[[n,length],[[v,t],[v,m2],[v,n]]]]]],[[]]),notrace,noprotocol.\nhalt.\nswipl -G100g -T20g -L2g\n['../Text-to-Breasonings/text_to_breasonings.pl'].\ntime((N is 3,\nM is 16000,\ntexttobr2(N,",File2,",u,M),texttobr(N,",File2,",u,M))).\n['../texttobr2qb'].\ntexttobr2(3).\nhalt."],List_bb1).
+	term_to_atom(C,List_a2),
+	string_atom(S,List_a2),
+	!.
 
 grammar_logic_to_alg(Sentence1,B) :- %% Not by multi-sentence algorithms, just by sentence
 	atom_string(Sentence0,Sentence1),
 	downcase_atom(Sentence0,Sentence01),
 	atom_string(Sentence01,Sentence02),
+	
 	SepandPad="&#@~%`$?-+*^,()|.:;=_/[]<>{}\n\r\s\t\\\"!'0123456789",
 	
-	Connectors=
+	/*Connectors=
 		["the","a","i","on","with","of","an","for","to",
 		"was","were","and","in","my","from","out","by"],
+	*/
 	split_string(Sentence02,SepandPad,SepandPad,Sentence2),
 	%subtract(Sentence2,Connectors,Sentence3),
 	%%length(Sentence3,Length),
@@ -184,28 +176,31 @@ grammar_logic_to_alg(Sentence1,B) :- %% Not by multi-sentence algorithms, just b
 	%% () add generated data
 	%%write_commands(Length,[],Commands), %% sentence alg
 	
-	generate_sentences(Sentence2,[],Sentence_a,30), %% detail 	sentences
-	append(Sentence3,Sentence_a,Sentence4),
-	findall([*,Sentence1,a_alg(Sentence5),b_alg(Sentence5,a),bb_alg(Sentence6)],(member(Sentence4a,Sentence4),make_lists(Sentence4a,[],Sentence5),Sentence5=[_|Sentence6]),B),!.
+	findall(B1,(member(S,Sentence2),
+
+	Num=79,
+	retractall(words_for_sent(_)),
+	assertz(words_for_sent([])),
+	%Nums2 is ceiling(Num),
+	length(List2,Num),
+	findall(W1,(member(_,List2),
+	words_for_sent(WS),
+	find_until_passes((
+	generate_sentence([A12],Sentence),
+	[_,_,W1,_W2|_]=Sentence,
+	not(member(W1,WS))
+	)),
+	append(WS,[W1],WS1),
+	retractall(words_for_sent(_)),
+	assertz(words_for_sent(WS1))
+	),W4),
+	append([S],W4,B1)
+	),B2),
+	flatten(B2,B),
+	
+	!.
 	  %% detail algs
 	
-generate_sentences(_Sentence3,Sentence_a,Sentence_a,0) :- !.
-generate_sentences(Sentence3,Sentence_a1,Sentence_a2,N1) :-
-	random_member(Item,Sentence3),
-	generate_sentence(Item,Sentence_a3),
-	append(Sentence_a1,[Sentence_a3],Sentence_a4),
-	N2 is N1-1,
-	generate_sentences(Sentence3,Sentence_a4,Sentence_a2,N2).
-
-%%write_commands(0,Commands,Commands) :- !.
-%%write_commands(Length1,Commands1,Commands2) :-
-%%	Length2 is Length1-1.
-%%	append(Commands1,[[[n,member2],[[v,Length1],[v,Length2]]]).
-	%%[[n,equals1],[[v,Length2],[[v,a***],[v,b]]]]].
-
-%%[a,b]
-%%[c,d]
-
 generate_sentence(Item,Sentence) :-
 	random_member(Grammar1,[[n,v,n],[n,v,a,n],[v,n],[v,a,n]]),
 	brdict(BrDict012),
